97a59526e21c9ed151df0a0b42999acef56d0b44
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-02 14:31:26 +0545

    Refactor Welcome component UI

--- hello-world/src/Welcome.jsx
+++ hello-world/src/Welcome.jsx
@@ -1,11 +1,14 @@
 
-
-
-
-function Welcome ()
+/*function Welcome ()
 {
   return <h2>Welcome back</h2>;
 
 
+}*/ // we can rewrite same function in arrow function
+
+ export const Welcome =()=>
+{
+    return <h2>Welcome back</h2>;
 }
-export default Welcome;
+
+/*export default Welcome;*/
