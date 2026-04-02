c07a4f6ca35ef30586e4123eb00d4fb95b7f8106
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-02 17:16:53 +0545

    Update Welcome component

--- hello-world/src/Welcome.jsx
+++ hello-world/src/Welcome.jsx
@@ -5,10 +5,21 @@
 
 
 }*/ // we can rewrite same function in arrow function
-
- export const Welcome =()=>
-{
-    return <h2>Welcome back</h2>;
-}
-
 /*export default Welcome;*/
+
+
+ /*export const Welcome =(props)=>
+{
+
+  console.log(props);
+    return <h2>Welcome ,{props.name} a.k.a {props.alias}</h2>;
+
+}*/
+
+export const Welcome =({name,alias})=>
+{
+
+
+    return <h2>Welcome ,{name} a.k.a {alias}</h2>;
+
+}
