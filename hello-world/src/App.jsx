1d004939b5aa384ac3083eaf2d03d04ce1c5d41f
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-02 14:09:55 +0545

    Feat:Implemeted child component

---
+++ hello-world/src/App.jsx
@@ -0,0 +1,32 @@
+
+
+import Welcome from './Welcome';
+import './App.css'
+
+
+
+
+
+function Button ()
+{
+  return <button>Click me</button>;
+}
+ function App()
+ {
+  return (
+    
+           <div>
+            <h1>
+              Just sarting react
+            </h1>
+            <Welcome />
+            <Button />
+           </div>
+               
+           
+  
+   
+  );
+}
+
+export default App
---
+++ hello-world/src/Welcome.jsx
@@ -0,0 +1,11 @@
+
+
+
+
+function Welcome ()
+{
+  return <h2>Welcome back</h2>;
+
+
+}
+export default Welcome;
