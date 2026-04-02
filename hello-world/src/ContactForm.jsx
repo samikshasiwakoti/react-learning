88291af675a806a291ac4a1c923da6ae01a48d59
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-02 16:02:00 +0545

    Feat:Implemented about the tag in the react every tag must be closed

---
+++ hello-world/src/ContactForm.jsx
@@ -0,0 +1,10 @@
+export const ContactForm=()=>
+{
+    return(
+        <form>
+            <input type ="text" placeholder ="your name"/>
+            <br/>
+            <input type ="email" placeholder ="your email"/>
+        </form>
+    )
+}
